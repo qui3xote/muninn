52c6084630918e33b6fa4c7fb4d15fee1d517ed3
Tom <tmellis@gmail.com>
2023-03-10 15:34:29 -0500

    Added listener url and basic subscription handler

--- app/main.py
+++ app/main.py
@@ -1,14 +1,30 @@
 import os
 
 from mastodon import Mastodon
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from dotenv import load_dotenv
+from typing import Optional
 
 class Account(BaseModel):
     # see AccountSerializer for the full list of properties: 
     #   https://github.com/mastodon/mastodon/blob/main/app/serializers/rest/admin/account_serializer.rb
     username: str
+
+class Notification(BaseModel):
+    id: str
+    created_at: str
+    type: str
+    account: Account
+    status: Optional(Status)
+
+class Status(BaseModel):
+    id: str
+    uri: str
+    url: str
+    account: Account
+    in_reply_to_id: Optional(str)
+    in_reply_to_account_id: Optional(str)
 
 
 class AccountCreatedEvent(BaseModel):
@@ -16,9 +32,37 @@ class AccountCreatedEvent(BaseModel):
     created_at: str
     object: Account
 
+class NotificationEvent(BaseModel):
+    notification: dict
+
+def get_connections():
+    
+
+async def lifespan():
+    app.state.mastodon = Mastodon(
+        access_token=os.environ.get("ACCESS_TOKEN"),
+        api_base_url=os.environ.get("API_BASE_URL")
+    )
+    app.state.instance_name = os.environ.get("INSTANCE_NAME"),
+    app.state.admin_account_name = os.environ.get("ADMIN_ACCOUNT_NAME"),
+    app.state.onboarding_link = os.environ.get("ONBOARDING_LINK")
+    
+    muninn_listener_url = os.environ.get("MUNINN_LISTENER_URL")
+    subscription_keys = app.state.mastodon.push_subscription_generate_keys()
+
+    app.state.push_subscription_set(
+        muninn_listener_url, 
+        subscription_keys,
+        follow_events=True,
+        mention_events=True
+    )
+
+    yield
+    # do something after shutdown
+
 load_dotenv()
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
@@ -27,23 +71,21 @@ def read_root():
 
 
 @app.post("/")
-def root(event: AccountCreatedEvent):
+def root(event: AccountCreatedEvent, request: Request):
     #   Set up Mastodon
-    mastodon = Mastodon(
-        access_token=os.environ.get("ACCESS_TOKEN"),
-        api_base_url=os.environ.get("API_BASE_URL"),
-        instance_name=os.environ.get("INSTANCE_NAME"),
-        admin_account_name=os.environ.get("ADMIN_ACCOUNT_NAME"),
-        onboarding_link=os.environ.get("ONBOARDING_LINK")
-    )
 
     account_id = f"@{event.object.username}"
     message = account_id + \
-        f" Welcome to {instance_name}! I'm your friendly welcome bot, Muninn. \n" \
+        f" Welcome to {app.state.instance_name}! I'm your friendly welcome bot, Muninn. \n" \
         "Don't forget to set up your profile and write an #introduction post. \n" \
-        f"Reach out to our admin, {admin_account_name}, if you have any questions or concerns. \n" \
-        f"New to Mastodon? Here's a quick getting started guide: {onboarding_link}"
+        f"Reach out to our admin, {app.state.admin_account_name}, if you have any questions or concerns. \n" \
+        f"New to Mastodon? Here's a quick getting started guide: {app.stateonboarding_link}"
 
     mastodon.status_post(message, visibility='direct')
 
     return {"message": f"Sent welcome message to {account_id}."}
+
+
+@app.post("/listener")
+def notification(event: NotificationEvent):
+    return {"message": f"Received {event.json()}"}
